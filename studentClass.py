e9fe5ff53177b19359dff2750693685914977544
zaidAlrabaia <alrabaia@kth.se>
2025-04-21 20:56:11 +0200

    Finished adding error handling AKA task 2

--- studentClass.py
+++ studentClass.py
@@ -4,14 +4,15 @@ class student:
     numberOfStudents = 0
     csvFilePath = "studentList.csv"
     def __init__(self, firstName: str, familyName: str, personalID: str, program: str, grade: str):
-        
-        student.validate_student_input(firstName,familyName,personalID,program,grade)
+        try:
+            student.validate_student_input(firstName,familyName,personalID,program,grade)
+        except Exception as e:
+            print("There was a validation error: " + str(e))
         self.__firstName = firstName
         self.__familyName = familyName
         self.__personalID = personalID
         self.__program = program
         self.__grade = grade
-        student.numberOfStudents += 1
 
         found = student.studentFinder(personalID, student.csvFilePath)
 
@@ -28,15 +29,18 @@ class student:
     def get_numberOfStudents():
         student.numberOfStudents = 0
         if os.path.exists(student.csvFilePath):
-            with open(student.csvFilePath, "r") as file:
-                reader = csv.reader(file)
-                next(reader,None)
-                for row in reader:
-                    if not row:
-                        continue
-                    else:
-                        student.numberOfStudents += 1
-        print(student.numberOfStudents)
+            try:
+                with open(student.csvFilePath, "r") as file:
+                    reader = csv.reader(file)
+                    next(reader,None)
+                    for row in reader:
+                        if not row:
+                            continue
+                        else:
+                            student.numberOfStudents += 1
+                print(student.numberOfStudents)
+            except Exception as e:
+                print("Something went wrong: " + str(e))
 
     def get_firstName(self):
         return self.__firstName
@@ -51,7 +55,10 @@ class student:
     
     @staticmethod
     def studentFinder(personalID, csvFile):
-        if os.path.exists(csvFile):
+        if not os.path.exists(csvFile):
+            print("Hello g! File not found do sumn bout it")
+            return False
+        try:
             with open(csvFile, "r") as file:
                 reader = csv.reader(file)
                 for row in reader:
@@ -59,6 +66,8 @@ class student:
                         continue
                     if row[2] == personalID:
                         return True
+        except Exception as e:
+            print("Error reading from file: " + str(e))
         return False
     
     @staticmethod
@@ -90,97 +99,156 @@ class student:
             print("File does not exist")
             return
         updated_list = []
-        with open(file_path, "r") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if  row and any(cell.strip() for cell in row):
-                    updated_list.append(row)
-        with open(file_path, "w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerows(updated_list)
+        try:
+            with open(file_path, "r") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if  row and any(cell.strip() for cell in row):
+                        updated_list.append(row)
+        except Exception as e:
+                print("Error reading file: " + str(e))
+                return
+        try:
+            with open(file_path, "w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerows(updated_list)
+        except Exception as e:
+            print("There was an error writing the table: " + str(e))
+            return
     
     @staticmethod
     def viewAllStudents(file_path):
+        if not os.path.exists(file_path):
+            print("MY BROTHER! FILE NOT FOUND")
+            return
+        
         columnWidths = []
-        with open(file_path, "r") as file:
-            reader = csv.reader(file)
-            headers = next(reader, None)
-            rows = list(reader)
-            columns = list(zip(*([headers]+rows)))
-            for col in columns:
-                max_len = max(len(cell) for cell in col)
-                columnWidths.append(max_len)
-            header_row = []
-            for i, cell in enumerate(headers):
-                header_row.append(cell.ljust(columnWidths[i]))
-            print(" | ".join(header_row))
-            print("-" * sum(columnWidths) + "-"*12)
+        try:
+            with open(file_path, "r") as file:
+                reader = csv.reader(file)
+                headers = next(reader, None)
 
-            for row in rows:
-                padded_row = []
-                for i, cell in enumerate(row):
-                    padded_row.append(cell.ljust(columnWidths[i]))
-                print(" | ".join(padded_row))
+                if not headers:
+                    print("The file is most likely empty and would cause a crash")
+                    return
+                
+                rows = list(reader)
+                columns = list(zip(*([headers]+rows)))
+
+                for col in columns:
+                    max_len = max(len(cell) for cell in col)
+                    columnWidths.append(max_len)
+
+                header_row = []
+                for i, cell in enumerate(headers):
+                    header_row.append(cell.ljust(columnWidths[i]))
+                print(" | ".join(header_row))
+                print("-" * sum(columnWidths) + "-"*12)
+
+                for row in rows:
+                    padded_row = []
+                    for i, cell in enumerate(row):
+                        padded_row.append(cell.ljust(columnWidths[i]))
+                    print(" | ".join(padded_row))
+
+        except Exception as e:
+            print("IDK what happened but sumn went wrong with the view all students method: " + str(e))
     
     @staticmethod
     def addStudent(file_path):
-            name = input("Please write student first name: ")
-            familyName = input("Please write student family name: ")
-            personalID = input("Please write student personal Id (10 or 12 digits): ")
-            program = input("Please write student program: ")
-            grade = input("Please write student grade: ")
-            student(name,familyName,personalID,program,grade)
-        
+            try:
+                name = input("Please write student first name: ").strip()
+                familyName = input("Please write student family name: ").strip()
+                personalID = input("Please write student personal Id (10 or 12 digits): ").strip()
+                program = input("Please write student program: ").strip()
+                grade = input("Please write student grade: ").strip()
+                
+                if student.studentFinder(personalID, file_path):
+                    print("Student already exists")
+                    return
+                
+                student(name,familyName,personalID,program,grade)
+                student.numberOfStudents += 1
+
+            except ValueError as ve:
+                print("Value Error: " + str(ve))
+            except Exception as e:
+                print("Something went wrong with the user input: " + str(e))
 
     @staticmethod
     def searchForStudent(file_path):
-        personalID = input("Please write the personal ID number: ")
-        with open(file_path, "r") as file:
-            reader = csv.reader(file)
-            headers = next(reader, None)
-            rows = list(reader)
-            columns = list(zip(*[headers] + rows))
-            padded_column = []
-            for col in columns:
-                max_len = max(len(cell) for cell in col)
-                padded_column.append(max_len)
-            padded_header = []
-            
-            found = False
-            for i, cell in enumerate(headers):
-                        padded_header.append(cell.ljust(padded_column[i]))
-            for row in rows:
-                if row[2].strip() == personalID.strip():
-                    found = True
-                    padded_row= []
-                    print(" | ".join(padded_header))
-                    for i, cell in enumerate(row):
-                        padded_row.append(cell.ljust(padded_column[i]))
-                    print(" | ".join(padded_row))
-            if not found:
-                print("Student not found")
-
-
+        if not os.path.exists(file_path):
+            print("File not found")
+            return
+        
+        if os.path.getsize(file_path) == 0:
+            print("file is empty")
+            return
+        
+        personalID = input("Please write the personal ID number: ").strip()
+    
+        try:
+            with open(file_path, "r") as file:
+                reader = csv.reader(file)
+                headers = next(reader, None)
+                if not headers:
+                    print("Header row is missing")
+                    return
+                rows = list(reader)
+                columns = list(zip(*[headers] + rows))
+                padded_column = []
+                for col in columns:
+                    max_len = max(len(cell) for cell in col)
+                    padded_column.append(max_len)
+                padded_header = []
+                
+                found = False
+                for i, cell in enumerate(headers):
+                            padded_header.append(cell.ljust(padded_column[i]))
+                for row in rows:
+                    if row[2].strip() == personalID.strip():
+                        found = True
+                        padded_row= []
+                        print(" | ".join(padded_header))
+                        for i, cell in enumerate(row):
+                            padded_row.append(cell.ljust(padded_column[i]))
+                        print(" | ".join(padded_row))
+                        return padded_row
+                if not found:
+                    print("Student not found")
+        except Exception as e:
+            print("Sumn went wrong: " + str(e))
 
     @staticmethod
     def deleteStudent(file_path):
-        personalID = input("Please write the personal ID of the student you want to delete: ")
-        confirm = input("Are you sure you want to delete the student with personal ID: (yes/no) " + personalID).lower()
-        if confirm == "yes":
-            student.deleteStudent(student.csvFilePath, personalID)
+        if not os.path.exists(file_path):
+            print("File not found")
+            return
         
+        if os.path.getsize(file_path) == 0:
+            print("File is empty broski")
+            return
 
-            # Step 1: Read all rows first
+        personalID = input("Please write the personal ID of the student you want to delete: ").strip()
+        confirm = input("Are you sure you want to delete the student with personal ID: (yes/no) " + personalID).lower()
+
+        if confirm != "yes":
+            print("Deletion canceled")
+            return
+
+        updated_list = []
+        deleted = False
+
+        try:
             with open(file_path, "r") as file:
                 reader = csv.reader(file)
                 header = next(reader, None)
                 for row in reader:
-                    if row and row[2] != personalID:
+                    if row and row[2].strip() != personalID.strip():
                         updated_list.append(row)
-                    elif row and row[2] == personalID:
+                    elif row and row[2].strip() == personalID.strip():
                         deleted = True
 
-            # Step 2: Overwrite with updated data
             with open(file_path, "w", newline="") as file:
                 writer = csv.writer(file)
                 if header:
@@ -191,7 +259,7 @@ class student:
                 print(f" Student with ID {personalID} was deleted.")
             else:
                 print(f" Student with ID {personalID} not found.")
-        else:
-            print("Deletion cancled")
-            updated_list = []
-            deleted = False
+        
+        except Exception as e:
+            print(" Something went wrong during deletion:", str(e))
+
